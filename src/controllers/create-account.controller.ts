6fc7aee5f5bc02d70c92645311b6fb290a2471d3
Julio Tavares <juliotavares197@gmail.com>
2023-12-11 21:26:43 -0300

    feat: create account

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,50 @@
+import { ZodValidationPipe } from '@/pipes';
+import { PrismaService } from '@/prisma';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { z } from 'zod';
+
+const CreateAccountSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateAccountInput = z.infer<typeof CreateAccountSchema>;
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(CreateAccountSchema))
+  async handle(@Body() body: CreateAccountInput) {
+    const { name, email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with same email already exists');
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
